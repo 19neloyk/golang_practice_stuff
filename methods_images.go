b0f28412b9837b2b78696e59e76a2541a4db2d0b
Neloy Kundu <19neloyk@lrei.org>
2021-04-17 15:23:02 -0400

    Create methods_images.go

---
+++ methods_images.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct{
+	dim_x int
+	dim_y int
+}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, i.dim_x, i.dim_y)
+}
+
+func (i Image) At(x int ,y int) color.Color {
+	v := uint8((x*y^(1/2))/2)
+	return color.RGBA{v, v, 255, 255} 
+}
+
+func main() {
+	m := Image{dim_x:500,dim_y:500}
+	pic.ShowImage(m)
+}
